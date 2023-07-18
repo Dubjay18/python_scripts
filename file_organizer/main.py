f8d92783909d4bd089a6dbd789cf41c76d73e4fd
Dubjay18 <jejeniyi7@gmail.com>
2023-07-18 23:19:39 +0100

    file organizer done

--- file_organizer/main.py
+++ file_organizer/main.py
@@ -1,3 +1,17 @@
 import os
 import shutil
 
+path =  input("Enter the name of the directory to be sorted :- ")
+files = os.listdir(path)
+
+for file in files:
+    filename,extension = os.path.splitext(file)
+    extension = extension[1:]
+    
+    if os.path.exists(path+'/'+extension):
+        print("Path already exists")
+        shutil.move(path+'/'+file,path+'/'+extension+'/'+file)
+    else:
+        os.makedirs(path+'/'+extension)
+        print("Path created")
+        shutil.move(path+'/'+file,path+'/'+extension+'/'+file)
