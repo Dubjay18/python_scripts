b9cbf8956fbe9cd806dda2ba79ad8a4eaf5f59dd
Dubjay18 <jejeniyi7@gmail.com>
2023-07-20 02:26:27 +0100

    fix:added comments

--- file_organizer/custom.py
+++ file_organizer/custom.py
@@ -1,19 +1,24 @@
+# Importing necessary modules
 import os
 import shutil
 
-path =  input("Enter the name of the directory to be sorted :- ")
-text = input("files to sort:-")
+# Getting the directory path and file name from user
+path =  input("Enter the name of the directory to be sorted: ")
+text = input("Enter the name of the file to sort: ")
 files = os.listdir(path)
 
+# Looping through each file in the directory
 for file in files:
-    filename,extension = os.path.splitext(file)
-    extension = extension[1:]
-    print(text in filename)
+    filename, extension = os.path.splitext(file)
+    extension = extension[1:] # Removing the "." from the extension
+    
+    # Checking if the file name contains the text to sort
     if text in filename:
+        # Checking if a directory with the same name as text exists
         if os.path.exists(path+'/'+text):
             print("Path already exists")
-            shutil.move(path+'/'+file,path+'/'+text+'/'+file)
+            shutil.move(path+'/'+file, path+'/'+text+'/'+file) # If it exists, move the file to that directory
         else:
-            os.makedirs(path+'/'+text)
+            os.makedirs(path+'/'+text) # If it doesn't exist, create a new directory with that name
             print("Path created")
-            shutil.move(path+'/'+file,path+'/'+text+'/'+file)
+            shutil.move(path+'/'+file, path+'/'+text+'/'+file) # Then move the file to that directory
--- file_organizer/main.py
+++ file_organizer/main.py
@@ -1,17 +1,24 @@
+# Importing necessary modules
 import os
 import shutil
 
+# Getting the directory path from user
 path =  input("Enter the name of the directory to be sorted :- ")
+
+# Getting all the files in the directory
 files = os.listdir(path)
 
+# Looping through each file in the directory
 for file in files:
-    filename,extension = os.path.splitext(file)
-    extension = extension[1:]
+    # Splitting the filename and extension
+    filename, extension = os.path.splitext(file)
+    extension = extension[1:] # Removing the "." from the extension
     
+    # Checking if a directory with the same extension exists
     if os.path.exists(path+'/'+extension):
         print("Path already exists")
-        shutil.move(path+'/'+file,path+'/'+extension+'/'+file)
+        shutil.move(path+'/'+file,path+'/'+extension+'/'+file) # If it exists, move the file to that directory
     else:
-        os.makedirs(path+'/'+extension)
+        os.makedirs(path+'/'+extension) # If it doesn't exist, create a new directory with that extension
         print("Path created")
-        shutil.move(path+'/'+file,path+'/'+extension+'/'+file)
+        shutil.move(path+'/'+file,path+'/'+extension+'/'+file) # Then move the file to that directory
